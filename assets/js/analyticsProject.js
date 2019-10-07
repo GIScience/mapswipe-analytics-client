eefb8dc39b0afc978820bd6620b77cf5a242d63a
Herfort <h9b@geog>
2019-10-07 17:13:13 +0200

    add project page for analytics

--- assets/js/analyticsProject.js
+++ assets/js/analyticsProject.js
@@ -4,7 +4,6 @@ function makeplot() {
 
 function processData(allRows) {
 
-  console.log(allRows);
   var x = [], y = [], standard_deviation = [];
 
   for (var i=0; i<allRows.length; i++) {
@@ -12,7 +11,6 @@ function processData(allRows) {
     x.push( row['day'] );
     y.push( row['cumulative_groups_finished_count'] );
   }
-  console.log( 'X',x, 'Y',y, 'SD',standard_deviation );
   makePlotly( x, y, standard_deviation );
 }
 
@@ -39,7 +37,50 @@ function initProjectMap() {
   }
 
 
+function populateProjectDataTable(projectId, datasetNames) {
+  console.log('added projects')
+
+  var tableRef = document.getElementById('projectDataTable').getElementsByTagName('tbody')[0];
+
+  datasetNames.forEach(function(element) {
+    tr = tableRef.insertRow();
+
+    td = document.createElement('td')
+    td.innerHTML = projectId
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerHTML = element.name
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerHTML = element.datatype
+    tr.appendChild(td)
+
+    td = document.createElement('td')
+    td.innerHTML = element.description
+    tr.appendChild(td)
+  })
+
+  $('#projectDataTable').DataTable();
+  $('.dataTables_length').addClass('bs-select');
+
+}
+
+
 function initAnalyticsProject() {
+  var projectId = '5519'
+
+
+  var datasetNames = [
+    {"name":"Raw Results", "description":"Raw Result contain information on MapSwipe Tasks where at least one user submitted a results. If there are several results per task by different users, the individual results are aggregated. For each task a yes_count, maybe_count and bad_imagery_count and further values are provided.", "datatype":"geoJSON"},
+    {"name":"Yes Maybe Results", "description":"This dataset contains all results where at least one mapswipe user submitted a yes or maybe classification. The output dataset depicts the union of all selected results.", "datatype":"geoJSON"},
+    {"name":"Bad Imagery Results", "description":"This dataset contains all results where at least one mapswipe user submitted a bad_image classification and no other classifications are more frequently submitted. The output dataset depicts the union of all selected results.", "datatype":"geoJSON"},
+    {"name":"HOT Tasking Manager Geometries", "description":"This dataset contains shapes that are ready to use in the HOT Tasking Manager. Currently, the geometries consist of maximum 15 MapSwipe Tasks, where at least one user indicated the presence of a building. The filter criteria and size of the tasks can be adapted on request.", "datatype":"geoJSON"},
+    {"name":"Progress and Contributors by date", "description":"Number of contributors and project progress on a daily basis.", "datatype":"CSV"},
+  ]
+
   initProjectMap();
-  makeplot()
+  makeplot();
+  populateProjectDataTable(projectId, datasetNames);
 }
